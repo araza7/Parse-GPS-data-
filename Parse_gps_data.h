603b60fa9ef247d886a748d8b5141522f402ef96
Ali Raza <86835504+araza7@users.noreply.github.com>
2023-06-17 14:11:40 +0500

    Parse_gps_data.h

--- Parse_gps_data.h
+++ Parse_gps_data.h
@@ -1,1 +1,17 @@
+#ifndef GPS_PARSER_H
+#define GPS_PARSER_H
 
+#include <string>
+using namespace std;
+//std namespace
+// Struct to store GPS parameters
+struct GPSData {
+    string time;
+    double latitude;
+    double longitude;
+};
+
+// Function to parse GPS data
+bool parse_gps_data(const string& packet, GPSData& data);
+
+#endif // GPS_PARSER_H
